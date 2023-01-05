81c7017a2e2504cdd9bbe4e86d70889ee95d49b6
llll <llll@DESKTOP-IKQAJ77>
2023-01-05 10:14:41 +0900

    add werewolf

---
+++ jan04/src/cookie/WerewolfCookie.java
@@ -0,0 +1,11 @@
+package cookie;
+
+public class WerewolfCookie extends Cookie implements Third{
+
+	@Override
+	public void ThirdButton() {
+		// TODO Auto-generated method stub
+		System.out.println("werewolf form");
+	}
+
+}
