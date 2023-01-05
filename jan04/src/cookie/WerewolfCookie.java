7c9ae92736a8313b79429ef73cfa68a582ff2eef
llll <1230pf@gmail.com>
2023-01-05 10:19:25 +0900

    fix werewolf

--- jan04/src/cookie/WerewolfCookie.java
+++ jan04/src/cookie/WerewolfCookie.java
@@ -5,7 +5,7 @@ public class WerewolfCookie extends Cookie implements Third{
 	@Override
 	public void ThirdButton() {
 		// TODO Auto-generated method stub
-		System.out.println("werewolf form");
+		System.out.println("Activate Werewolf form");
 	}
 
 }
